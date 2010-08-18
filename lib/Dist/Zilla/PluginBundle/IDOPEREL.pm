783fb3611893259941a3b71a2b9a1ae1f786480d
Ido Perlmuter <idope@bezeq.co.il>
2010-08-18 11:03:19 +0300

    woooooooooeeeeeeee

--- lib/Dist/Zilla/PluginBundle/IDOPEREL.pm
+++ lib/Dist/Zilla/PluginBundle/IDOPEREL.pm
@@ -18,61 +18,6 @@ use Dist::Zilla::Plugin::GithubMeta;
 use Dist::Zilla::Plugin::TestRelease;
 use Dist::Zilla::Plugin::ReadmeFromPod;
 
-sub bundle_config {
-	my ($self, $section) = @_;
-	my $class = ref($self) || $self;
-
-	my $arg = $section->{payload};
-
-	my @plugins = Dist::Zilla::PluginBundle::Filter->bundle_config(
-		{
-			name    => "$class/Classic",
-			payload => {
-				bundle => '@Classic',
-				remove => [qw/Readme/],
-			}
-		}
-	);
-
-	# params for AutoVersion
-	my $major_version = defined $arg->{major_version} ? $arg->{major_version} : 1;
-
-	# params
-
-	my $prefix = 'Dist::Zilla::Plugin::';
-	my @extra = map { [ "$class/$prefix$_->[0]" => "$prefix$_->[0]" => $_->[1] ] } (
-		[ AutoPrereq          => { skip  => $arg->{auto_prereq_skip} } ],
-		[ AutoVersion         => { major => $major_version } ],
-		[ CheckChangeLog      => {} ],
-		[ CheckChangesTests   => {} ],
-		[ CompileTests        => {} ],
-		[ HasVersionTests     => {} ],
-		[ MetaTests           => {} ],
-		[ MetaJSON            => {} ],
-		[ ManifestSkip        => {} ],
-		[ MetaResources       => \%meta_resources ],
-		[ MinimumVersionTests => {} ],
-		[ ModuleBuild         => {} ],
-		[ NextRelease         => {} ],
-		[ PodWeaver           => {} ],
-		[ PortabilityTests    => {} ],
-		[ Prepender           => {} ],
-		[ ReadmeFromPod       => {} ],
-		[ Manifest            => {} ],   # should come last
-	);
-
-	push (@plugins, @extra);
-
-	# add git plugins
-	push (@plugins,	Dist::Zilla::PluginBundle::Git->bundle_config({ name => "$section->{name}/Git",	payload => {} });
-
-	eval "require $_->[1]; 1;" or die for @plugins; ## no critic Carp
-
-	return @plugins;
-}
-
-__PACKAGE__->meta->make_immutable;
-
 =head1 NAME
 
 Dist::Zilla::PluginBundle::IDOPEREL - IDOPEREL's plugin bundle for Dist::Zilla.
@@ -119,6 +64,52 @@ This bundle provides the following plugins and bundles:
 	[GithubMeta]
 	[ReadmeFromPod]
 	[TestRelease]
+
+=head1 INTERNAL METHODS
+
+=head2 bundle_config
+
+=cut
+
+sub bundle_config {
+	my ($self, $section) = @_;
+	my $class = ref($self) || $self;
+
+	my $arg = $section->{payload};
+
+	my @plugins = Dist::Zilla::PluginBundle::Filter->bundle_config(
+		{
+			name    => "$class/Classic",
+			payload => {
+				bundle => '@Classic',
+				remove => [qw/Readme/],
+			}
+		}
+	);
+
+	# params
+
+	my $prefix = 'Dist::Zilla::Plugin::';
+	my @extra = map { [ "$class/$prefix$_->[0]" => "$prefix$_->[0]" => $_->[1] ] } (
+		[ AutoPrereq          => { skip  => $arg->{auto_prereq_skip} } ],
+		[ MetaJSON            => {} ],
+		[ MinimumPerl         => {} ],
+		[ NextRelease         => {} ],
+		[ ReadmeFromPod       => {} ],
+		[ TestRelease         => {} ],
+	);
+
+	push (@plugins, @extra);
+
+	# add git plugins
+	push (@plugins,	Dist::Zilla::PluginBundle::Git->bundle_config({ name => "$section->{name}/Git",	payload => {} }));
+
+	eval "require $_->[1]; 1;" or die for @plugins; ## no critic Carp
+
+	return @plugins;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 =head1 AUTHOR
 
